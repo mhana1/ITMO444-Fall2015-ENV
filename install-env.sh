f7d27213f6ec8c0ba52b2a2a43fd7eaaba4adf4f
Mossa Hana <mhana1@hawk.iit.edu>
2015-10-04 17:37:51 -0500

    Edited version of install-env.sh

--- install-env.sh
+++ install-env.sh
@@ -6,7 +6,7 @@ sudo apt-get install -y  apache2 git
 git clone https://github.com/mhana1/ITMO444-Fall2015-APP.git
 
 mv ./ITMO444-Fall2015-APP/images /var/www/html/images
-mv ./ITMO444-Fall2015-App/css /var/www/html/css
+mv ./ITMO444-Fall2015-APP/css /var/www/html/css
 mv ./ITMO444-Fall2015-APP/*.html /var/www/html
 
 echo "Hello!" > var/tmp/hello.txt
